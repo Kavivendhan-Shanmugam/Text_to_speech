574d8ae9cabc9e9c486de411ae046ea9c24497b3
SABARI S B <earc.ethereum@gmail.com>
2025-11-15 15:13:27 +0530

    first push

---
+++ app/src/main/java/com/example/text_to_speech/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.text_to_speech;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn ;
+    EditText edit ;
+    TextToSpeech tts;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btn = (Button)findViewById(R.id.button);
+        edit = (EditText)findViewById(R.id.editText);
+        
+        tts = new TextToSpeech(getApplicationContext(),
+                new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        if (status == TextToSpeech.SUCCESS){
+                            tts.setLanguage(Locale.ENGLISH);
+                        }
+                    }
+                });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = edit.getText().toString();
+                tts.speak(s, TextToSpeech.QUEUE_FLUSH, null, null);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+}
